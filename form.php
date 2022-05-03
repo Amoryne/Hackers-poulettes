c36de4fd8138b3abf3085691ec64f5197925603d
Amoryne <amoryne.jolimont@gmail.com>
2022-05-03 14:24:30 +0200

    final

--- form.php
+++ form.php
@@ -1,10 +1,26 @@
-<?php
-//INITIALISATION PHP MAILER
+<?php 
+session_start(); 
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-require 'phpmailer/src/Exception.php';
-require 'phpmailer/src/PHPMailer.php';
-require 'phpmailer/src/SMTP.php';
+
+//TEST RECUP DATA FORM
+
+$name=$_POST['name'];
+$lastname=$_POST['lastname'];
+$country=$_POST['country'];
+$email=$_POST['email'];
+$gender=$_POST['gender'];
+$subject=$_POST['subject'];
+
+
+
+
+// //INITIALISATION PHP MAILER
+// use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\Exception;
+// require 'phpmailer/src/Exception.php';
+// require 'phpmailer/src/PHPMailer.php';
+// require 'phpmailer/src/SMTP.php';
 
 //ON VERIFIE SI LES CHAMPS SONT REMPLIS
 
@@ -16,7 +32,7 @@ if(isset($_POST['name']) && !empty($_POST['name'])
 && isset($_POST['gender'])
 && isset($_POST['message']) && !empty($_POST['message'])) {
 
-//ON NETTOIE LES DATA ENTREES PAR L'UTILISATEUR
+// ON NETTOIE LES DATA ENTREES PAR L'UTILISATEUR
 
     $name= strip_tags($_POST['name']);
     $lastname= strip_tags($_POST['lastname']);
@@ -24,18 +40,102 @@ if(isset($_POST['name']) && !empty($_POST['name'])
     $message=strip_tags($_POST['message']);
     $email= strip_tags($_POST['email']);
 
-$name=$_POST['name'];
+
+    
+    echo '<pre>';
+    var_dump($_POST);
+    die();
 
     // // EXECUTE FUNCTION -> RECUPERER DATA FORMULAIRE
     //                     -> SEND TO EMAIL
-if(isset($_POST['submit'])){
-    echo '<pre>';
-    var_dump($_POST['name']);
-    die();
-}
 
 
-}
+require 'vendor/autoload.php';
+
+
+   
+    try{   
+    $mail = new PHPMailer();
+    $mail->isSMTP(true);
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->Host = 'smtp.mailtrap.io';
+    $mail->Port = 2525;
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->SMTPAuth = true;
     
+    $mail->Username = '3c3bcb0059fda7';
+    $mail->Password = '80ad540886ee20';
+    
+    $mail->setFrom($email);
+    $mail->addAddress('amoryne.jolimont@gmail.com', 'Amo Joli');
+    //CONTENT 
+    $mail->isHTML(true);
+    $mail->Subject = $subject;
+    $mail->Body = $message;
+    $mail->AltBody = $message;
+    echo 'mail send';
+    $mail->send();
+    echo 'mail send';
+  }  catch (Exception $e) {
+    echo 'Exception reçue : ',  $e->getMessage(), "\n";
+}
+
+
+
+
+
+}else{
+
+ if(empty($_POST["name"])){
+    $_SESSION['empty_name'] = "Please enter you name in the blanks field !";
+
+    //NE MARCHE PAS SI MESSAGE OU ECHO AVANT
+        header('Location: index.php');
+ }if(empty($_POST["lastname"])){
+    $_SESSION['empty_lastname'] = "Please enter you lastname in the blanks field !";
+
+    //NE MARCHE PAS SI MESSAGE OU ECHO AVANT
+        header('Location: index.php'); 
+        
+}if(empty($_POST["email"])){
+    $_SESSION['empty_email'] = "Please enter you email in the blanks field !";
+
+    //NE MARCHE PAS SI MESSAGE OU ECHO AVANT
+        header('Location: index.php'); 
+}if(empty($_POST["country"])){
+    $_SESSION['empty_country'] = "Please enter you country in the blanks field !";
+
+    //NE MARCHE PAS SI MESSAGE OU ECHO AVANT
+        header('Location: index.php'); 
+}if(empty($_POST["message"])){
+    $_SESSION['empty_message'] = "Please enter you message in the blanks field !";
+
+    //NE MARCHE PAS SI MESSAGE OU ECHO AVANT
+        header('Location: index.php'); 
+}if(empty($_POST["gender"])){
+    $_SESSION['empty_gender'] = "Please pick you gender !";
+
+    //NE MARCHE PAS SI MESSAGE OU ECHO AVANT
+        header('Location: index.php'); 
+}if(empty($_POST["subject"])){
+    $_SESSION['empty_subject'] = "Please select your subject message in the text area !";
+
+    //NE MARCHE PAS SI MESSAGE OU ECHO AVANT
+        header('Location: index.php'); 
+}
+}
+
+
+// else if(empty($_POST['name'])
+// && empty($_POST['lastname'])
+// && empty($_POST['country'])
+// && empty($_POST['subject'])
+// && empty($_POST['email'])
+// && empty($_POST['gender'])
+// && empty($_POST['message'])) {
+//     $_SESSION['empty_all'] = "Please fill all the blanck area !";
+
+//     //NE MARCHE PAS SI MESSAGE OU ECHO AVANT
+//         header('Location: index.php'); }
 
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,128 +17,185 @@
     <title>Hackers Poulettes</title>
 </head>
 <body>
+<?php
+//MESSAGE ALERT NOUVEAU PRODUIT AJOUTE
+if(!empty($_SESSION['empty_name'])){
+    echo    '<div class="alert alert-success" role="alert">
+            '. $_SESSION['empty_name'].'
+            </div>';
+    $_SESSION['empty_name'] = "";
+}
+?>
+<?php
+//MESSAGE ALERT NOUVEAU PRODUIT AJOUTE
+if(!empty($_SESSION['empty_lastname'])){
+    echo    '<div class="alert alert-success" role="alert">
+            '. $_SESSION['empty_lastname'].'
+            </div>';
+    $_SESSION['empty_lastname'] = "";
+}
+?>
+<?php
+//MESSAGE ALERT NOUVEAU PRODUIT AJOUTE
+if(!empty($_SESSION['empty_all'])){
+    echo    '<div class="alert alert-success" role="alert">
+            '. $_SESSION['empty_all'].'
+            </div>';
+    $_SESSION['empty_all'] = "";
+}
+?>
+<?php
+//MESSAGE ALERT NOUVEAU PRODUIT AJOUTE
+if(!empty($_SESSION['empty_email'])){
+    echo    '<div class="alert alert-success" role="alert">
+            '. $_SESSION['empty_email'].'
+            </div>';
+    $_SESSION['empty_email'] = "";
+}
+?>
+<?php
+//MESSAGE ALERT NOUVEAU PRODUIT AJOUTE
+if(!empty($_SESSION['empty_country'])){
+    echo    '<div class="alert alert-success" role="alert">
+            '. $_SESSION['empty_country'].'
+            </div>';
+    $_SESSION['empty_country'] = "";
+}
+?>
+<?php
+//MESSAGE ALERT NOUVEAU PRODUIT AJOUTE
+if(!empty($_SESSION['empty_gender'])){
+    echo    '<div class="alert alert-success" role="alert">
+            '. $_SESSION['empty_gender'].'
+            </div>';
+    $_SESSION['empty_gender'] = "";
+}
+?>
+<?php
+//MESSAGE ALERT NOUVEAU PRODUIT AJOUTE
+if(!empty($_SESSION['empty_subject'])){
+    echo    '<div class="alert alert-success" role="alert">
+            '. $_SESSION['empty_subject'].'
+            </div>';
+    $_SESSION['empty_subject'] = "";
+}
+?>
+<?php
+//MESSAGE ALERT NOUVEAU PRODUIT AJOUTE
+if(!empty($_SESSION['empty_message'])){
+    echo    '<div class="alert alert-success" role="alert">
+            '. $_SESSION['empty_message'].'
+            </div>';
+    $_SESSION['empty_message'] = "";
+}
+?>
+
+
+
    <header class="container-fluid myheader" >
-       
-    
-       
-    <nav class="navbar navbar-expand-lg navbar-light">
-  <!-- <a class="navbar-brand" href="#">Navbar</a> -->
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav gap-4 ms-4">
-      <li class="nav-item ">
-        <a class="nav-link" href="#">Home </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Products</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link active" href="#">Contact</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-       
-<div class="logo d-flex flex-row-reverse ">
-    <img src="assets/img/hackers-poulette-logo.png" class="img-fluid justify-content-end imglogo " alt="Hackers Poulette Logo"></div>   
-</header>
+    	<nav class="navbar navbar-expand-lg navbar-light">
+  			<!-- <a class="navbar-brand" href="#">Navbar</a> -->
+  			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    			<span class="navbar-toggler-icon"></span>
+  			</button>
+  			<div class="collapse navbar-collapse" id="navbarNav">
+    			<ul class="navbar-nav gap-4 ms-4">
+    	  			<li class="nav-item ">
+    	    			<a class="nav-link" href="#">Home </a>
+    	  			</li>
+    	  			<li class="nav-item">
+    	  			  	<a class="nav-link" href="#">Products</a>
+    	  			</li>
+    	  			<li class="nav-item">
+    	  			  	<a class="nav-link" href="#">Pricing</a>
+    	  			</li>
+    	  			<li class="nav-item active">
+    	  			  	<a class="nav-link active" href="#">Contact</a>
+    	  			</li>
+    			</ul>
+  			</div>
+		</nav>
+		<div class="logo d-flex flex-row-reverse ">
+    		<img src="assets/img/hackers-poulette-logo.png" class="img-fluid justify-content-end imglogo " alt="Hackers Poulette Logo">
+		</div>   
+	</header>
+	<main>		
+		<div class="position-form">
+ 			<div class="contact">
+     			<div>
+    				<p>Contact</p>
+    				<span class="us"><p>Us...</p></span>
+     			</div>
+			</div>   
+   			<div class="container-formulaire ">
+				<form class="form row g-9 gap-3" action="form.php" method="post">
 
+					<!-- Gender -->
+					<div class="gender col-ls">
+    					<label for="gender">Gender</label>
+						<div class="checkbox">
+							<input type="radio" name="gender" value="men">
+							<label for="men">Mr</label><br>
+							<input type="radio" name="gender" value="women">
+							<label for="women">Mlle</label>
+						</div>
+					</div>
 
-<div class="position-form">
- <div class="contact">
-     <div>
-    <p>Contact</p>
-    <span class="us"><p>Us...</p></span>
-     </div>
-</div>   
-   <div class="container-formulaire ">
-<form class="form row g-9 gap-3" action="form.php" method="post">
+					<!-- Name -->
+					<div class="col-lg">
+						<label for="name" class="col-form-label">Name</label>
+					</div>
+					<div class="col-auto">
+						<input type="text" name="name" placeholder="Enter your name" id="name" class="myinput form-control" aria-describedby="name">
+					</div>
 
+					<!-- Lastname -->
+					<div class="col-lg">
+						<label for="lastname" class="col-form-label">Lastname</label>
+					</div>
+					<div class="col-auto">
+						<input type="text" name="lastname" placeholder="Enter your lastname" id="lastname" class="myinput form-control" aria-describedby="lastname">
+					</div>
 
+					<!-- email -->
+					<div class="col-lg">
+						<label for="email" class="col-form-label">E-mail</label>
+					</div>
+					<div class="col-auto">
+						<input type="email" name="email" placeholder="@Enter your e-mail " id="email" class="myinput form-control" aria-describedby="email">
+					</div>
 
+					<!-- country -->
+					<div class="col-lg">
+						<label for="country" class="col-form-label">Country</label>
+					</div>
+					<div class="col-auto">
+						<input type="text" name="country" placeholder="Where are you from ?" id="country" class="myinput form-control" aria-describedby="country">
+					</div>
 
-<div class="gender col-ls">
-    <label for="gender">Gender</label>
-
-
-<div class="checkbox">
-    <input type="radio"  name="thegender" value="Man">
-    <label for="men">Mr</label><br>
-    <input type="radio" name="thegender" value="Woman">
-    <label for="women">Mlle</label>
-</div></div>
-<!-- Name -->
-  
-<div class="col-lg">
-    <label for="name" class="col-form-label">Name</label>
-  </div>
-  <div class="col-auto">
-    <input type="text" placeholder="Enter your name" id="name" class="myinput form-control" aria-describedby="name">
-  </div>
-  <!-- Lastname -->
-  <div class="col-lg">
-    <label for="lastname" class="col-form-label">Lastname</label>
-  </div>
-  <div class="col-auto">
-    <input type="text" placeholder="Enter your lastname" id="lastname" class="myinput form-control" aria-describedby="lastname">
-  </div>
-
-  <!-- email -->
-  <div class="col-lg">
-    <label for="email" class="col-form-label">E-mail</label>
-  </div>
-  <div class="col-auto">
-    <input type="email" placeholder="@Enter your e-mail " id="email" class="myinput form-control" aria-describedby="email">
-  </div>
-  <!-- country -->
-  <div class="col-lg">
-    <label for="country" class="col-form-label">Country</label>
-  </div>
-  <div class="col-auto">
-    <input type="text" placeholder="Where are you from ?" id="country" class="myinput form-control" aria-describedby="country">
-  </div>
-
-  
-    <!-- Subjects -->
-    <div class="col-lg">
-    <label for="subject" class="col-form-label">Subject</label>
-  </div>
-  <div class="myselect col-5">
-   
-  
-    <select class="form-select" aria-label="Default select example">
-  <option selected>Choose Subject</option>
-  <option value="1">About Product</option>
-  <option value="2">Technical Problem</option>
-  <option value="3">Job offer</option>
-  
-</select>
-
- </div>
-<div class="col-lg">
-    <label for="message" class="col-form-label">Message</label>
-  </div>
-  <div class="col-auto">
-    <textarea rows="2" placeholder="Enter your message" cols="21" id="message" class="form-control" aria-describedby="message"></textarea>
-  </div>
-  
-<div class="button">
-  <input class="submit" type="submit" name="submit" value="SEND"> 
-</div>
-</form>
-
-
-
-
-  
-</div>
-
-
-</body>
-
+					<!-- Subjects -->
+					<div class="col-lg">
+						<label for="subject" class="col-form-label">Subject</label>
+					</div>
+					<div class="myselect col-5">
+						<select class="form-select" name="subject" aria-label="Default select example">
+							<option selected>Choose Subject</option>
+							<option value="About Product">About Product</option>
+							<option value="Technical Problem">Technical Problem</option>
+							<option value="Job offer">Job offer</option>
+						</select>
+					</div>
+					<div class="col-lg">
+						<label for="message" class="col-form-label">Message</label>
+					</div>
+					<div class="col-auto">
+						<textarea rows="2" name="message" placeholder="Enter your message" cols="21" id="message" class="form-control" aria-describedby="message"></textarea>
+					</div>
+					<div class="button">
+						<input class="submit" type="submit" name="submit" value="SEND"> 
+					</div>
+				</form>
+			</div>
+		</main>
+	</body>
 </html>
